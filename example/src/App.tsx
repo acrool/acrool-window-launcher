8e8f6586a697b1e5b2b5124f717184064df4b053
imagine10255 <imagine10255@gmail.com>
2024-06-25 15:40:54 +0800

    modify READEME

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,21 +7,30 @@ import Github from './assets/github.svg?react';
 
 function App() {
 
+    const renderHeader = () => {
 
-    return (
-        <div className="App">
-            <a href="https://github.com/acrool/acrool-window-launcher" target="_blank" rel="noopener noreferrer">
+        const repositoryUrl = 'https://github.com/acrool/acrool-window-launcher';
+        const name = 'Acrool Window Launcher';
+
+        return <>
+            <a href={repositoryUrl} target="_blank" rel="noopener noreferrer">
                 <Github width={40} height={40}/>
             </a>
 
-            <div style={{textAlign: 'center', border: '1px solid #606060', display: 'flex', flexDirection: 'column', padding: '20px', margin: '20px 0'}}>
-                <img src="/logo.svg" style={{height: '100px'}} alt="Acrool Window Launcher"/>
-                <div style={{fontSize: '40px', color: '#fff', fontWeight: 700}}>Acrool Window Launcher</div>
+            <div className="banner-wrapper">
+                <img src="/logo.svg" alt={name}/>
+                <h1>{name}</h1>
             </div>
+        </>;
+    };
 
+
+
+    return (
+        <div className="App">
+            {renderHeader()}
 
             <Example/>
-
         </div>
     );
 }
