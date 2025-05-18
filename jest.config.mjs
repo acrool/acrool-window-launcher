9c347bef174786a1f2b4040bb2d48b22d956d7f5
imagine10255 <imagine10255@gmail.com>
2025-05-18 15:55:14 +0800

    add playwright.yml test

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,11 @@ export default {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     testMatch: ['**/*.(spec|test).[jt]s?(x)'],
+    testPathIgnorePatterns: [
+        "/node_modules/",
+        "/playwright/",
+        "/example/",
+    ],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1'
     },
